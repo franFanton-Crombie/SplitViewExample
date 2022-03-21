f9af73cfdd5d9c3f7dc5c37d8d1a279901bc4242
franFanton-4R <francisco.fanton@4rsoluciones.com>
2022-03-21 10:55:38 -0300

    change code

--- src/SplitScreen/index.js
+++ src/SplitScreen/index.js
@@ -1,16 +1,24 @@
 import * as React from 'react';
-import SplitView from '../components/SplitView';
 import {isIpad} from '../../helpers/constants';
 import MasterSplitStack from './stacks/MasterSplitStack';
 import DetailsSplitStack from './stacks/DetailsSplitStack';
 import useSystemBack from './useSystemBack';
+import {StyleSheet, View} from 'react-native';
+import MenuVertical from '../screens/MenuVertical';
 
 const SplitScreen = () => {
   if (isIpad()) {
     useSystemBack();
 
     return (
-      <SplitView master={<MasterSplitStack />} detail={<DetailsSplitStack />} />
+      <View style={styles.root}>
+        <View style={styles.masterView}>
+          <MenuVertical />
+        </View>
+        <View style={styles.detailView}>
+          <DetailsSplitStack />
+        </View>
+      </View>
     );
   }
 
@@ -18,3 +26,14 @@ const SplitScreen = () => {
 };
 
 export default SplitScreen;
+
+const styles = StyleSheet.create({
+  root: {flex: 1, flexDirection: 'row'},
+  masterView: {flex: 1, maxWidth: 150, borderWidth: 1, borderColor: 'blue'},
+  detailView: {
+    flex: 1,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: 'blue',
+  },
+});
